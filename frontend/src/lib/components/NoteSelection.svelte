cc95827383fc6b1922d7dae4b98a5598586b0464
euisungkang <euisungkang@gmail.com>
2025-03-26 11:35:18 -0400

    feat: prototype frontend

---
+++ frontend/src/lib/components/NoteSelection.svelte
@@ -0,0 +1,7 @@
+<script lang='ts'>
+</script>
+
+<div class="h-full w-1/2 flex items-center justify-center">
+  
+</div>
+
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+import Algorithms from '$lib/components/Algorithms.svelte';
+import NoteSelection from '$lib/components/NoteSelection.svelte';
+import Sliders from '$lib/components/Sliders.svelte';
+
+
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</svelte:head>
+
+<div class="h-screen w-screen flex flex-col items-center justify-center bg-[#121212]">
+  <div class="h-1/2 w-1/2 flex items-center justify-center space-x-12">
+    <Algorithms/>
+    <Sliders/>
+  </div>
+  <div class="h-1/2 w-full flex items-center justify-center">
+    <NoteSelection/>
+  </div>
+</div>
