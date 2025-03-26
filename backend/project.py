c6408dabc2beee72c7d5518a20d43b910a401fca
Antero Mejr <mail@antr.me>
2025-03-26 11:03:49 -0400

    Starting API

--- backend/project.py
+++ backend/project.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
 """CS 150 final project"""
 
-def main():
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/api/update-base-melody", methods=["POST"])
+def update_base_melody():
+    pass
+
+@app.route("/api/add-algorithm", methods=["POST"])
+def add_algorithm():
     pass
 
 if __name__ == "__main__":
-    main()
+    app.run()
