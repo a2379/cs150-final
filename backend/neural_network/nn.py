02be11062a8d1f3163e43483d16e9137b340b67c
euisungkang <euisungkang@gmail.com>
2025-04-17 13:08:52 -0400

    feat: working version (jazz, rock, gospel)

--- backend/neural_network/nn.py
+++ backend/neural_network/nn.py
@@ -1,18 +1,15 @@
 import os
+import glob
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import music21 as m21
-
-import pretrained_metadata as metadata
 
 # Load built-in corpus
 chorales = m21.corpus.chorales.Iterator()
 
 # Notes are encoded in tuples of three possible and unique notes
 encodedNotes = {}
-
-DEFAULT_EPOCH = 250
 
 
 # Simple LSTM-based model to predict harmony from melody
@@ -21,26 +18,28 @@ class HarmonyModel(nn.Module):
         super(HarmonyModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, vocab_size)
+        self.harmony_fc = nn.Linear(hidden_dim, vocab_size)
+        self.bass_fc = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, x):
         x = self.embedding(x)
         x, _ = self.lstm(x)
-        x = self.fc(x)
-        return x
+        h_output = self.harmony_fc(x)
+        b_output = self.bass_fc(x)
+        return h_output, b_output
 
 
 # Save recently trained model to pretrained path
-def save_model(model):
-    torch.save(model.state_dict(), "./pretrained")
-    with open("./pretrained_metadata.py", "w") as file:
+def save_model(model, genre):
+    torch.save(model.state_dict(), f"./{genre}/pretrained")
+    with open(f"./{genre}/pretrained_metadata.py", "w") as file:
         file.write(f"CORPUS_ENCODED_SIZE={len(encodedNotes)}\n")
         file.write(f"NOTE_TO_IDX={encodedNotes}\n")
-        file.write("""PRETRAINED_PATH='./pretrained'\n""")
+        file.write(f"PRETRAINED_PATH='./{genre}/pretrained'\n")
 
 
 # Load pretrained model and model metadata
-def load_model():
+def load_model(metadata):
     model = HarmonyModel(metadata.CORPUS_ENCODED_SIZE)
     if os.path.exists(metadata.PRETRAINED_PATH):
         model.load_state_dict(torch.load(metadata.PRETRAINED_PATH))
@@ -50,53 +49,84 @@ def load_model():
         encodedNotes = metadata.NOTE_TO_IDX
     else:
         print("No pretrained model found. Training new model.")
-        model = train_network()
+        # model = train_network()
     return model
 
 
+def extract_pitches(obj):
+    if not obj:
+        return ()
+    if obj.isNote:
+        return obj.nameWithOctave
+    elif obj.isChord:
+        return tuple(p.nameWithOctave for p in obj.pitches)
+
+
 # Extracts melody-harmony pairs from the music21 corpus with harmony as chords
-def process_corpus_data():
+def process_corpus_data(midiPath):
     sequences = []
 
-    for chorale in chorales:
+    for fname in glob.glob(midiPath):
+        try:
+            stream = m21.converter.parse(fname)
+        except m21.midi.MidiException as _:
+            continue
+
         mNotes = []
         hNotes = []
+        bNotes = []
 
         # Store parts by name
-        parts = {"Soprano": None, "Alto": None, "Tenor": None, "Bass": None}
+        parts = {"Melody": None, "Harmony": None, "Bass": None}
 
         # Tranpose Note objects from music21 corpus
-        for part in chorale.parts:
-            if part.partName:
-                for voice in parts.keys():
-                    if voice in part.partName:
-                        parts[voice] = part.flatten().notes
+        try:
+            for part in stream.parts:
+                # Melody
+                if any(s in part.partName for s in ["Percuss", "Drum"]):
+                    continue
+                elif any(s in part.partName for s in ["Bass", "Brass"]):
+                    parts["Bass"] = part.flatten().notes
+                elif any(s in part.partName for s in ["Piano", "Melody"]):
+                    parts["Melody"] = part.flatten().notes
+                else:
+                    parts["Harmony"] = part.flatten().notes
+        except:
+            continue
+
+        print(fname)
 
         # Tranpose melody-harmony from Note objects
-        if parts["Soprano"]:
-            for note in parts["Soprano"]:
-                if note.isNote:
-                    mNotes.append(note.nameWithOctave)
+        if parts["Melody"]:
+            for note in parts["Melody"]:
+                melodyNote = extract_pitches(note)
+                if not melodyNote:
+                    continue
 
-                    # Harmony = Alto + Tenor + Bass
-                    chord = []
-                    for voice in ["Alto", "Tenor", "Bass"]:
-                        if parts[voice]:
-                            matching_notes = parts[voice].getElementsByOffset(
-                                note.offset, mustBeginInSpan=False
-                            )
-                            (
-                                chord.append(
-                                    matching_notes[0].nameWithOctave
-                                    if matching_notes
-                                    else "Rest"
-                                )
-                            )
+                harmNote = ()
+                if parts["Harmony"]:
+                    matching_notes = parts["Harmony"].getElementsByOffset(
+                        note.offset, mustBeginInSpan=False
+                    )
+                    if not matching_notes:
+                        continue
+                    harmNote = extract_pitches(matching_notes[0])
 
-                    hNotes.append(tuple(chord))
+                bassNote = ()
+                if parts["Bass"]:
+                    matching_notes = parts["Bass"].getElementsByOffset(
+                        note.offset, mustBeginInSpan=False
+                    )
+                    if not matching_notes:
+                        continue
+                    bassNote = extract_pitches(matching_notes[0])
 
-        if mNotes and hNotes and len(mNotes) == len(hNotes):
-            sequences.append((mNotes, hNotes))
+                mNotes.append(melodyNote)
+                hNotes.append(harmNote)
+                bNotes.append(bassNote)
+
+        if mNotes and hNotes and bNotes and len(mNotes) == len(hNotes) == len(bNotes):
+            sequences.append((mNotes, hNotes, bNotes))
 
     return sequences
 
@@ -112,17 +142,18 @@ def encode_sequences(sequences):
         (
             torch.tensor([encodedNotes[n] for n in melody], dtype=torch.long),
             torch.tensor([encodedNotes[n] for n in harmony], dtype=torch.long),
+            torch.tensor([encodedNotes[n] for n in bass], dtype=torch.long),
         )
-        for melody, harmony in sequences
+        for melody, harmony, bass in sequences
     ]
 
     return encodedData
 
 
 # Train a new PyTorch model with hyperparams and optimizers per the tutorials
-def train_network():
+def train_network(midiPath):
     # Generate encoded training data from music21 corpus
-    sequences = process_corpus_data()
+    sequences = process_corpus_data(midiPath)
     encodedData = encode_sequences(sequences)
 
     # Hyperparameters per the PyTorch tutorials
@@ -132,20 +163,35 @@ def train_network():
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # Training loop by # of epochs
-    epochs = DEFAULT_EPOCH
-    for epoch in range(epochs):
+    epoch = 0
+    overfit_count = 0
+    anchor_loss = float("inf")
+    while overfit_count < 3:
+        # for epoch in range(epochs):
+        epoch += 1
         CELoss = 0
-        for melody, harmony in encodedData:
+        for melody, harmony, bass in encodedData:
             optimizer.zero_grad()
-            output = model(melody.unsqueeze(0))
-            loss = criterion(output.squeeze(0), harmony)
+            h_output, b_output = model(melody.unsqueeze(0))
+            hloss = criterion(h_output.squeeze(0), harmony)
+            bloss = criterion(b_output.squeeze(0), bass)
+            loss = hloss + bloss
             loss.backward()
             optimizer.step()
             CELoss += loss.item()
 
+        avg_loss = CELoss / len(encodedData)
+
         # Useful for tracking training progress when training neural network
         # Cross-Entropy Loss of < 1.3 is achieved with DEFAULT_EPOCH
-        print(f"Epoch {epoch + 1}/{epochs}, Loss: {CELoss / len(encodedData):.4f}")
+        print(f"Epoch {epoch}, Loss: {avg_loss:.4f}")
+
+        if avg_loss > anchor_loss:
+            overfit_count += 1
+        else:
+            overfit_count = 0
+
+        anchor_loss = avg_loss
 
     return model
 
@@ -159,9 +205,14 @@ def generate_harmony(model, melodySequence):
 
     # Predict harmony notes from sequence of melody notes
     with torch.no_grad():
-        output = model(melodyTensor).squeeze(0)
-        predictedNotes = torch.argmax(output, dim=1)
+        harmony_output, bass_output = model(melodyTensor)
+        harmony_output = harmony_output.squeeze(0)
+        harmony_predicted = torch.argmax(harmony_output, dim=1)
+        bass_output = bass_output.squeeze(0)
+        bass_predicted = torch.argmax(bass_output, dim=1)
 
         # Invert k,v pairs of encodedNotes to derive final pitches
         encodedNotesInverse = {i: note for note, i in encodedNotes.items()}
-        return [encodedNotesInverse[i.item()] for i in predictedNotes]
+        return [encodedNotesInverse[i.item()] for i in harmony_predicted], [
+            encodedNotesInverse[i.item()] for i in bass_predicted
+        ]
