986e60b4b99e1a654f853606766843bd31afbd2a
euisungkang <euisungkang@gmail.com>
2025-04-17 15:11:00 -0400

    pre integration

--- backend/neural_network/script.py
+++ backend/neural_network/script.py
@@ -32,66 +32,66 @@ def generate_harmony(melody_pitches, genre):
         print(f"'{genre}' is an invalid genre.")
 
 
-def main():
-    genre = "rock"
+# def main():
+#     genre = "rock"
 
-    # start_time = time.time()
-    # model = nn.train_network(f"./{genre}/*.midi")
-    # nn.save_model(model, f"{genre}")
-    # end_time = time.time()
-    # elapsed_time = end_time - start_time
-    # print(f"Elapsed time: {elapsed_time / 60:.4f} minutes")
-    # return
+# start_time = time.time()
+# model = nn.train_network(f"./{genre}/*.midi")
+# nn.save_model(model, f"{genre}")
+# end_time = time.time()
+# elapsed_time = end_time - start_time
+# print(f"Elapsed time: {elapsed_time / 60:.4f} minutes")
+# return
 
-    melody = m21.stream.Part()
-    melody.insert(0, m21.instrument.Piano())
-    harmony = m21.stream.Part()
-    harmony.insert(0, m21.instrument.Piano())
-    bass = m21.stream.Part()
-    bass.insert(0, m21.instrument.Piano())
-    bass.append(m21.clef.BassClef())
+# melody = m21.stream.Part()
+# melody.insert(0, m21.instrument.Piano())
+# harmony = m21.stream.Part()
+# harmony.insert(0, m21.instrument.Piano())
+# bass = m21.stream.Part()
+# bass.insert(0, m21.instrument.Piano())
+# bass.append(m21.clef.BassClef())
 
-    melody_pitches = [
-        ("C4", "E4", "G4"),
-        ("C4", "E4", "G4"),
-        ("C4", "E4", "G4"),
-        ("C4", "E4", "G4"),
-        ("E4", "G4"),
-        "E4",
-        "G4",
-        "E4",
-        "C4",
-        ("E4", "G4"),
-        ("E4", "G4"),
-        ("E4", "G4"),
-        ("E4", "G4"),
-        ("C4", "G4"),
-        ("C4", "G4"),
-        ("C4", "G4"),
-        ("C4", "G4"),
-        "C4",
-        "E4",
-        "G4",
-        "E4",
-        "C4",
-    ]
+# melody_pitches = [
+#     ("C4", "E4", "G4"),
+#     ("C4", "E4", "G4"),
+#     ("C4", "E4", "G4"),
+#     ("C4", "E4", "G4"),
+#     ("E4", "G4"),
+#     "E4",
+#     "G4",
+#     "E4",
+#     "C4",
+#     ("E4", "G4"),
+#     ("E4", "G4"),
+#     ("E4", "G4"),
+#     ("E4", "G4"),
+#     ("C4", "G4"),
+#     ("C4", "G4"),
+#     ("C4", "G4"),
+#     ("C4", "G4"),
+#     "C4",
+#     "E4",
+#     "G4",
+#     "E4",
+#     "C4",
+# ]
 
-    # harmony_pitches, bass_pitches = nn.generate_harmony(model, melody_pitches)
-    harmony_pitches, bass_pitches = generate_harmony(melody_pitches, genre)
-    print(melody_pitches)
-    print(harmony_pitches)
-    print(bass_pitches)
+# harmony_pitches, bass_pitches = nn.generate_harmony(model, melody_pitches)
+# harmony_pitches, bass_pitches = generate_harmony(melody_pitches, genre)
+# print(melody_pitches)
+# print(harmony_pitches)
+# print(bass_pitches)
+#
+# sections = (melody, harmony, bass)
+#
+# convert_to_music21(sections, melody_pitches, harmony_pitches, bass_pitches)
+#
+# s = m21.stream.Stream()
+# for section in sections:
+#     s.insert(0, section)
+#
+# s.show()
 
-    sections = (melody, harmony, bass)
 
-    convert_to_music21(sections, melody_pitches, harmony_pitches, bass_pitches)
-
-    s = m21.stream.Stream()
-    for section in sections:
-        s.insert(0, section)
-
-    s.show()
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
